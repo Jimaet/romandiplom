c8c7058336db0f437d678b05ecb9451a2cc295a5
Александр Быков <150475371+Jimaet@users.noreply.github.com>
2025-04-04 12:44:26 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,14 @@
-document.querySelectorAll('.button').forEach(button => {
-    button.addEventListener('click', () => {
-        alert(`Вы выбрали: ${button.dataset.name}`);
-    });
-});
+function showCategory(category) {
+    // Скрыть все категории
+    const categories = document.querySelectorAll('.category');
+    categories.forEach(cat => cat.style.display = 'none');
+    
+    // Показать выбранную категорию
+    const selectedCategory = document.getElementById(category);
+    selectedCategory.style.display = 'flex';
+}
+
+function openPage(page) {
+    alert("Открыта страница с кнопкой: " + page);
+    // Здесь можно добавлять переходы или дополнительные действия при выборе.
+}
